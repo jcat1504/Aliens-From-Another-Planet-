3815a963beacbfabb84196d96ea0e85ad1beb664
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-09-25 14:03:20 -0700

    Update ex45.rb
    Fully updated file with require commands.

--- ex45.rb
+++ ex45.rb
@@ -1,4 +1,12 @@
 require './enginemap.rb'
+require './characters.rb'
+require './beginning.rb'
+require './fishpond.rb'
+require './cabin.rb'
+require './treehouse.rb'
+require './bridge.rb'
+require './shrine.rb'
+require './ending.rb'
 
 a_map = Map.new('beginning')
 a_game = Engine.new()
